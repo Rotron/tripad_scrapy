a75d194c095b22ed1cc33e6309c2cf4fde0cf31f
Benjamin ETIENNE <benjamin.etienne@centraliens_lille.org>
2017-02-28 16:02:53 +0100

    Fixes

--- tripadvisor/spiders/hotels.py
+++ tripadvisor/spiders/hotels.py
@@ -1,6 +1,9 @@
 import scrapy
 from tripadvisor.items import HotelItem, RestaurantItem
 
+
+
+
 class HotelSpider(scrapy.Spider):
     name = 'hotel'
     start_urls = ['https://www.tripadvisor.fr/Hotels-g11038886-Hauts_de_France-Hotels.html']
@@ -14,7 +17,7 @@ class HotelSpider(scrapy.Spider):
         
         
     def parse_hotel(self, response):
-        for href in response.xpath("//div[@class='quote']/a/@href"):
+        for href in response.xpath("//div[contains(@class,'quote')]/a/@href"):
             url = response.urljoin(href.extract())
             yield(scrapy.Request(url, callback=self.parse_review))
             
@@ -71,7 +74,7 @@ class RestaurantSpider(scrapy.Spider):
             
     
     def parse_restaurant(self, response):
-        for href in response.xpath("//div[@class='quote']/a/@href"):
+        for href in response.xpath("//div[contains(@class,'quote')]/a/@href"):
             url = response.urljoin(href.extract())
             yield(scrapy.Request(url, callback=self.parse_review))
             
@@ -79,4 +82,39 @@ class RestaurantSpider(scrapy.Spider):
         if next_page:
             url = response.urljoin(next_page)
             yield(scrapy.Request(url, self.parse_restaurant))
+            
+            
+class AttractionSpider(scrapy.Spider):
+    name = 'activites'
+    start_urls = ['https://www.tripadvisor.fr/Attractions-g11038886-Activities-Hauts_de_France.html']
+
+    def parse_review(self, response):
+        item = HotelItem()
+        item['date'] = response.xpath("//span[contains(@class,'ratingDate')]/@content").extract_first()
+        item['content'] = response.xpath("//div[@class='entry']/p/text()").extract_first().replace("\n", "")
+        item['rating'] = response.xpath("////span[@class='rate sprite-rating_s rating_s']/img/@alt").extract_first()[0]
+        return(item)
+        
+        
+    def parse_activity(self, response):
+        for href in response.xpath("//div[contains(@class,'quote')]/a/@href"):
+            url = response.urljoin(href.extract())
+            yield(scrapy.Request(url, callback=self.parse_review))
+            
+        next_page = response.xpath("//a[@class='nav next rndBtn ui_button primary taLnk']/@href").extract_first()
+        if next_page:
+            url = response.urljoin(next_page)
+            yield(scrapy.Request(url, self.parse_activity))
+    
+            
+    def parse(self, response):
+        # Get the list of all activities
+        for href in response.xpath("//div[@class='property_title']/a/@href"):
+            url = response.urljoin(href.extract())
+            yield(scrapy.Request(url, callback=self.parse_activity))
+            
+        next_page = response.xpath("//a[@class='nav next ui_button primary taLnk']/@href").extract_first()
+        if next_page:
+            url = response.urljoin(next_page)
+            yield(scrapy.Request(url, self.parse))
             
